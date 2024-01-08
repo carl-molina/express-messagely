9bcc70930036229b9d0f8a58e5b0a5dddc9c99a6
Carl Molina <carlandrew.molina@gmail.com>
2024-01-08 11:38:11 -0800

    Added register user fn

--- models/user.js
+++ models/user.js
@@ -9,6 +9,18 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const results = await db.query(
+      `INSERT INTO users (username, first_name, last_name, phone)
+      VALUES ($1, $2, $3, $4)
+      RETURNING username, password, first_name, last_name, phone`,
+      [this.username,
+        this.password,
+        this.first_name,
+        this.last_name,
+        this.phone]
+    );
+    const { username, password, first_name, last_name, phone } = results.rows[0];
+    return { username, password, first_name, last_name, phone };
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
