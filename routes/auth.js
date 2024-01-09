bc7ef009fc9ced16b6ebe0d74b2dad4523ce04d2
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-08 19:50:20 -0500

    added post login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,30 @@
 "use strict";
 
+const { SECRET_KEY } = require("../config");
+const db = require("../db");
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+
 const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
+
+router.post('/login', async function (req,res){
+  if (req.body === undefined) throw new BadRequestError();
+  const {username, password} = req.body;
+
+  const user = User.authenticate(username, password);
+
+  if (!user) {
+    throw new UnauthorizedError('invalid username or password');
+  }
+
+  const token = jwt.sign({ username }, SECRET_KEY);
+
+  return res.json({ token });
+})
 
 
 /** POST /register: registers, logs in, and returns token.
