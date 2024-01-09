9a79a0829de6d4931f28e4aa9e72b34f7085590f
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-08 20:00:44 -0500

    added login/register auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const { SECRET_KEY } = require("../config");
+const { SECRET_KEY, JWT_OPTIONS } = require("../config");
 const db = require("../db");
 const { BadRequestError, UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
 const jwt = require('jsonwebtoken');
+
 
 const Router = require("express").Router;
 const router = new Router();
@@ -31,5 +32,15 @@ router.post('/login', async function (req,res){
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post('/register', async function (req, res){
+
+  if (req.body === undefined) throw new BadRequestError();
+  const {username, password, first_name, last_name, phone} = req.body;
+
+  const newUser = User.register(username, password, first_name, last_name, phone);
+
+  return jwt.sign(newUser, SECRET_KEY, JWT_OPTIONS);
+})
 
 module.exports = router;
